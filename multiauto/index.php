19216a5c4dc601020b4b0fd954922b55fa7c1174
marcocaza <44465310+marcocaza@users.noreply.github.com>
2025-05-09 21:06:21 -0500

    new

--- multiauto/index.php
+++ multiauto/index.php
@@ -110,25 +110,25 @@
 <main class="form-signin w-100 m-auto">
   <form>
     <img class="mb-4" src="" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Por favor Registrese si quiere iniciar Sesion</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <label for="floatingInput">Correo</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
+      <label for="floatingPassword">Contraseña</label>
     </div>
 
     <div class="form-check text-start my-3">
       <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
-        Remember me
+        Recuerdame
       </label>
     </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+    <button class="btn btn-primary w-100 py-2" type="submit">Iniciar Sesion</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2024</p>
   </form>
 </main>
 <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
